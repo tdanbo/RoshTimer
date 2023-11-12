59192b01f8d8a510553eb5e618e1efcb6af4b607
tdanbo <46909851+tdanbo@users.noreply.github.com>
2023-11-12 18:26:27 +0100

    .

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QWidget
 from PySide6.QtCore import Qt, QTimer
+from PySide6.QtGui import QMouseEvent
 
 roshan_dead = """
 QPushButton {
     background-color: #000000;
     color: #a1a1a1; /* White */
-    border: 2px solid #a1a1a1;
+    border: 1px solid #a1a1a1;
     font-size: 20px;
     font-weight: bold;
     background-image: url("icons/roshan_resized.jpg");
@@ -20,15 +21,20 @@ QPushButton {
     color: #54cc67; /* Moderate Green */
     font-size: 20px;
     font-weight: bold;
-    border: 2px solid #54cc67;
+    border: 1px solid #54cc67;
     background-image: url("icons/roshan_resized.jpg");
     background-repeat: no-repeat;
     background-position: center;
     text-shadow: 2px 2px 4px #000000; /* horizontal offset, vertical offset, blur radius, color */
 }
 """
+class NoRightClickButton(QPushButton):
+    def __init__(self, text:str, parent: QWidget) -> None:
+        super().__init__(text, parent)
 
-
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if event.button() != Qt.RightButton: # type: ignore
+            super().mousePressEvent(event)
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -42,7 +48,7 @@ class MainWindow(QMainWindow):
         self.button = self._create_rosh_button()
 
         # Position the button and label
-        button_x, button_y = 0, 1062  # Replace with desired coordinates for button
+        button_x, button_y = 2171, 1252  # Replace with desired coordinates for button
         self.button.move(button_x, button_y)
 
         # Set window properties
@@ -57,9 +63,9 @@ class MainWindow(QMainWindow):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_countdown)
 
-    def _create_rosh_button(self) -> QPushButton:
-        button = QPushButton("START TIMER!", self.centralWidget())
-        button.setFixedSize(325, 50)
+    def _create_rosh_button(self) -> NoRightClickButton:
+        button = NoRightClickButton("START TIMER!", self.centralWidget())
+        button.setFixedSize(389, 50)
         # Use the appropriate stylesheet here
         button.setStyleSheet(roshan_dead)
         button.clicked.connect(self.start_countdown)
@@ -73,10 +79,10 @@ class MainWindow(QMainWindow):
     def update_countdown(self) -> None:
         minutes, seconds = divmod(self.current_time, 60)
         self.button.setText(f"{minutes:02d}:{seconds:02d}")
-        if self.current_time == 480:  # 8 minutes remaining
+        if self.current_time == 180:  # 8 minutes passed (3 minutes remaining)
             # Change to the appropriate stylesheet and text
             self.button.setStyleSheet(roshan_up)
-            self.button.setText(f"{minutes:02d}:{seconds:02d}")
+            self.button.setText(f"Spawn can happen: {minutes:02d}:{seconds:02d}")
         if self.current_time <= 0:
             self.timer.stop()
             self.button.setStyleSheet(roshan_up)
